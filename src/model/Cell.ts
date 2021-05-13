5c596da0414afa9096d0b3ba0a13d76e4433c595
gcampini <campinigil@gmail.com>
2021-05-13 19:25:11 +0200

    improve

--- src/model/Cell.ts
+++ src/model/Cell.ts
@@ -23,9 +23,9 @@ export default class Cell {
         const cells = [];
         let current: Cell = this;
         while (current) {
+            cells.push(current);
             if (current.neighbors[direction] !== -1) {
                 const neighbor = game.cells[current.neighbors[direction]];
-                cells.push(neighbor);
                 current = neighbor;
             } else current = null;
         }
@@ -35,21 +35,29 @@ export default class Cell {
     public attraction(game: Game): number {
         let attraction = 0;
         if (this.tree(game)) return attraction;
-        attraction += this.richness / 3;
-        attraction -= this.shadow(game);
+        attraction += (this.richness / 3) * 3;
+        attraction += this.shadowAttraction(game);
         return attraction;
     }
 
     // between 0 and 1
-    public shadow(game: Game): number {
-        const directionMenace = 6 - game.sunDirection;
+    public shadowAttraction(game: Game): number {
         let total = 0;
-        const ombresPotentielles = this.ray(game, directionMenace).splice(0, 4).filter(o => !!o);
-        for (let i = 0; i < ombresPotentielles.length; i++) {
-            const tree = ombresPotentielles[i].tree(game);
-            if (tree) total += Math.min(4, 4 + tree.size - i);
+        for (let direction = 0; direction < 6; direction++) {
+            let subtotal = 0;
+            const voisins = this.ray(game, direction).splice(0, 4).filter(cell => !!cell);
+            for (let i = 0; i < voisins.length; i++) {
+                const tree = voisins[i].tree(game);
+                if (!tree) continue;
+                // Ombre qui affecte cette cell
+                subtotal -= Math.min(4, 4 + tree.size - i) / 4;
+                // Ombre projetée sur une autre cell
+                const cellSize = (this.tree(game) ? this.tree(game).size : 0);
+                subtotal += ((tree.isMine ? -1 : 1) * (4 + tree.size + cellSize - i) + 10) / 20;
+            }
+            total += (subtotal / voisins.length + 1) / 2;
         }
-        return total / 16;
+        return total / 6;
     }
 
 }
--- src/solvers/bronze.ts
+++ src/solvers/bronze.ts
@@ -4,7 +4,6 @@ import GrowAction from "../model/actions/GrowAction";
 import SeedAction from "../model/actions/SeedAction";
 import WaitAction from "../model/actions/WaitAction";
 import Game from "../model/Game";
-import Tree from "../model/Tree";
 import Solver from "./Solver";
 
 const roundMax = 23;
@@ -13,10 +12,17 @@ export default class BronzeSolver implements Solver {
 
     solve(game: Game): Action {
 
+        console.error(game.cells[0].shadowAttraction(game));
+
         const treesToComplete = game.myTrees.filter(tree => tree.size === 3 && !tree.isDormant);
-        if (game.mySun >= 4 && (treesToComplete.length > 10 * (1 - (game.round / roundMax)))) {
-            return new CompleteAction(treesToComplete[0].cell.index);
+        if (game.mySun >= 4 && (treesToComplete.length > 7 * (1 - (game.round / roundMax)))) {
+            return new CompleteAction(treesToComplete.sort((t1, t2) => t2.cell.shadowAttraction(game) - t1.cell.shadowAttraction(game))[0].cell.index);
         }
+
+        const treeToGrow = game.myTrees
+            .filter(tree => !tree.isDormant && tree.size < 3)
+            .sort((t1, t2) => t2.cell.richness - t1.cell.richness || t2.growCost(game) - t1.growCost(game))[0];
+        if (treeToGrow && treeToGrow.growCost(game) <= game.mySun) return new GrowAction(treeToGrow.cell.index);
 
         if (game.round >= 3 && game.myTrees.filter(tree => tree.size === 0).length === 0) {
             const seedActions = game.actions.filter(a => {
@@ -29,9 +35,6 @@ export default class BronzeSolver implements Solver {
             });
             if (seedActions.length > 0) return seedActions[0];
         }
-
-        const treeToGrow = game.myTrees.filter(tree => !tree.isDormant && tree.size < 3).sort((t1, t2) => t2.growCost(game) - t1.growCost(game))[0];
-        if (treeToGrow && treeToGrow.growCost(game) <= game.mySun) return new GrowAction(treeToGrow.cell.index);
 
         return new WaitAction();
 
